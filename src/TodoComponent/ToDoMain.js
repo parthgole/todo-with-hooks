b4ee5b4ccc65b633b800c0f0b2ac0c61ccbd4a78
parthgole <parthgole.1993@gmail.com>
2023-04-27 17:27:50 +0530

    stage2MovementOfItemWithChecked+DOMCSS+UpdateItem+DeleteItem

--- src/TodoComponent/ToDoMain.js
+++ src/TodoComponent/ToDoMain.js
@@ -4,12 +4,11 @@ import {useState} from 'react'
 function ToDoMain(){
     const[inputData,setInputData]=useState('')
     const[items,setItems]=useState([])
-    
+    const[replace,setReplace]=useState({id:null,item:'',status:null,toggle:true})
     const addItem=()=>{
         if(!inputData){
             
         }else{
-        // const newEntry=[].push({item:inputData,status:false})
             setItems([...items,{item:inputData,status:false}])
             setInputData('')
            }  
@@ -18,36 +17,65 @@ function ToDoMain(){
      setItems([])   
     }
     const DeleteItem=(id)=>{
-        console.log(id)
+        // console.log(id)
         const updatedList=items.filter((item,index)=>{
             return index !== id
         })
         setItems(updatedList)
     }
     const change=(e)=>{
-        console.log(e,e.target.id,e.target.checked)
-        const updatedList=items.filter((item,index)=>{
-            return item.status=(index === e.target.id?true:false)
-        })
-        setItems(updatedList)
-        console.log(updatedList)
-    
+        // console.log(e,e.target.id,e.target.checked)
+        const updatedList=[...items];
+        const Length=updatedList.length
+        if(e.target.checked){
+            updatedList.push(updatedList.splice(e.target.id, 1)[0])
+            updatedList[Length-1].status=true
+            setItems([...updatedList])
+
+        }else{
+            updatedList[e.target.id].status=false
+            updatedList.unshift(updatedList.splice(e.target.id, 1)[0])            
+            setItems(updatedList)
+        }                
+        // console.log(updatedList)
     }
+    const update=()=>{
+        const demo={...replace}
+        demo.toggle=false;
+        setReplace(demo)
+        // console.log(demo)
+        const updateList=[...items]
+        updateList[demo.id].item=inputData;
+        updateList[demo.id].status=false;
+        setItems(updateList)
+        setInputData('')
+        setReplace({id:null,item:'',status:null,toggle:true})
+        // console.log(items)
+
+    }
+    const edit=(u)=>{
+        // console.log("edit",u)
+        const updatedList=[...items];
+        const repl={id:u,item:updatedList[u].item,status:updatedList[u].status}
+        setInputData(updatedList[u].item); 
+        setReplace(repl)
+    }
+    // console.log(replace)   
     return(
         <>
         <input type='text' value={inputData} onChange={(e)=>setInputData(e.target.value)}/><br/><br/>
-        <button onClick={addItem}>AddItem</button>
+        {replace.toggle?<button onClick={addItem}>AddItem</button>:
+        <button onClick={update}>update</button>}
         <button onClick={reset}>Reset</button>
-            <ul style={{width:'40px',height:'auto',border:'1px solid black',margin:'30px auto 10px'}}>
+            <div style={{width:'40%',height:'auto',margin:'30px auto 10px'}}>
             {items.map((data,index)=>
            
             <ToDoUI   key={index} data={data.item} index={index} remove={DeleteItem} 
-            change={change} status={data.status}/>
+            change={change} status={data.status} edit={edit}/>
             
             )}
-         </ul>
-
-            </>
+         </div>
+      </>
                 )
               
             }
--- src/TodoComponent/ToDoUI.js
+++ src/TodoComponent/ToDoUI.js
@@ -1,11 +1,21 @@
-function ToDoUI({data,index,remove,change,status}){
+function ToDoUI({data,index,remove,change,status,edit}){
     return(
         <>
-           <li style={{textDecoration:status?'line-through':'',color:status?'blue':'black'}}>{data} 
+        <div style={{width:'100%',height:'auto',backgroundColor:'yellow',
+        border:'1px solid black',margin:'10px auto 10px'}}>
+        <div>
+           {index+1}..
+           <span style={{textDecoration:status?'line-through':'',
+           color:status?'blue':'black'           
+           }}>{data} 
+           </span>
+        </div><br/>
+        <div style={{justifyItems:'center'}}>
            <i className="fa-solid fa-trash"  onClick={()=>remove(index)}></i>
-           <input type="checkbox" id={index} onChange={(e)=>change(e)}/>
-           </li>
-
+           <input type="checkbox" id={index} onChange={(e)=>change(e)} checked={status}/>
+           <i className="fas fa-edit" onClick={()=>edit(index)}></i>
+        </div>
+        </div>
         </>
     )
 }
